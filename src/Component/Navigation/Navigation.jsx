4b14c63372667a202d1fcfcaa80d8a2c4da0e8f6
romanchenko31 <romanchenko.zhen@gmail.com>
2023-03-08 20:36:02 +0200

    add scroll menu

--- src/Component/Navigation/Navigation.jsx
+++ src/Component/Navigation/Navigation.jsx
@@ -1,30 +1,69 @@
 import style from "../Navigation/Navigation.module.css";
 import { NavLink } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+
+const dataMenu = [
+  {
+    id: 1,
+    label: "Main",
+    path: "",
+  },
+  {
+    id: 2,
+    label: "Portfolio",
+    path: "portfolio",
+  },
+  {
+    id: 3,
+    label: "About",
+    path: "about",
+  },
+  {
+    id: 4,
+    label: "Contacts",
+    path: "contacts",
+  },
+];
 
 const Navigation = () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const ul = useRef(null);
+  useEffect(() => {
+    console.log(x - y);
+    ul.current.style.transform = `translate3d(${(x - y) * -1}px,0,0)`;
+  }, [x, y]);
+
   return (
-    <ul className={style.list}>
-      <li className={style.item}>
-        <NavLink className={`${style.link} ${style.main}`} to="">
-          Main
-        </NavLink>
-      </li>
-      <li className={style.item}>
-        <NavLink className={`${style.link} ${style.portfolio}`} to="portfolio">
-          Portfolio
-        </NavLink>
-      </li>
-      <li className={style.item}>
-        <NavLink className={`${style.link} ${style.about}`} to="about">
-          About
-        </NavLink>
-      </li>
-      <li className={style.item}>
-        <NavLink className={`${style.link} ${style.contacts}`} to="contacts">
-          Contacts
-        </NavLink>
-      </li>
-    </ul>
+    <>
+      <div className={style.list}>
+        <ul
+          ref={ul}
+          onMouseEnter={(v) => {
+            console.log(v);
+          }}
+          onTouchStart={({ changedTouches }) => {
+            const { clientX } = changedTouches[0];
+            setX(clientX);
+          }}
+          onTouchEnd={({ changedTouches }) => {
+            const { clientX } = changedTouches[0];
+            setY(clientX);
+          }}
+          className={style.l}
+        >
+          {dataMenu.map((v) => {
+            return (
+              <li key={v.id} className={style.item}>
+                <NavLink className={`${style.link}`} to={v.path}>
+                  {v.label}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
   );
 };
 
