086c81c5514c93024e0b94b0801f23f5589de780
romanchenko31 <romanchenko.zhen@gmail.com>
2023-03-10 19:55:23 +0200

    fix: add library slider menu

--- src/Component/Navigation/Navigation.jsx
+++ src/Component/Navigation/Navigation.jsx
@@ -2,30 +2,39 @@ import style from "../Navigation/Navigation.module.css";
 import { NavLink } from "react-router-dom";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
+import img1 from "../../images/link.svg";
+import img2 from "../../images/livros.svg";
+import img3 from "../../images/music.svg";
+import img4 from "../../images/video.svg";
+
 const dataMenu = [
   {
     id: 1,
     key: 11,
     label: "Main",
     path: "",
+    img: img1,
   },
   {
     id: 2,
     key: 12,
     label: "Portfolio",
     path: "portfolio",
+    img: img2,
   },
   {
     id: 3,
     key: 13,
     label: "About",
     path: "about",
+    img: img3,
   },
   {
     id: 4,
     key: 14,
     label: "Contacts",
     path: "contacts",
+    img: img4,
   },
 ];
 
@@ -35,37 +44,31 @@ const Navigation = () => {
     rtl: false,
     slides: {
       perView: 3,
-      spacing: 10,
+      spacing: 20,
     },
   });
   return (
-    <div
-      style={{
-        width: "350px",
-        position: "absolute",
-        right: "0",
-        bottom: "0",
-        display: "flex",
-
-        overflow: "hidden",
-      }}
-    >
-      <ul ref={sliderRef} className={`keen-slider`}>
-        {dataMenu.map((v) => {
-          return (
-            <li
-              style={{ boxShadow: "0px 15px 30px rgba(25, 31, 61, 0.1)" }}
-              className="keen-slider__slide number-slide1"
-              key={v.id}
-            >
-              <NavLink className={style.link} to={v.path}>
-                {v.label}
-              </NavLink>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <ul ref={sliderRef} className={`${style.list} keen-slider`}>
+      {dataMenu.map((value) => {
+        return (
+          <li
+            className={`${style.item} keen-slider__slide number-slide1`}
+            key={value.id}
+          >
+            <NavLink className={style.link} to={value.path}>
+              <img
+                className={style.img}
+                width="33"
+                height="33"
+                src={value.img}
+                alt="img"
+              />
+              {value.label}
+            </NavLink>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
